2cdc96935b2052041d3f3605593f9403e72e6909
Guilherme Fernandes Do bem <leciguilherme@gmail.com>
2024-12-22 20:58:55 -0300

    Remove line Duplicate

--- data/data_treatment.py
+++ data/data_treatment.py
@@ -5,25 +5,25 @@ import pandas as pd
     e depois e salvando em um arquivo csv com o mesmo nome''' 
 
 def tratamento_dos_dados():
-    # ACTIVITIES
+    # ACTIVITIES    
     
     df_activities = pd.read_excel('./xlsx/moskit_activities.xlsx')
-
     df_activities = df_activities.fillna(value=0, inplace=False )
-
-
-
+    
+    # Removendo as linhas duplicadas
+    
+    df_activities.drop_duplicates(inplace=True)
     print(df_activities)
-
-
+    
     #Salvando o arquivo csv  
     df_activities.to_csv('./csv/moskit_activities.csv', index=False)
 
     # COMPANIES
     df_companies = pd.read_excel('./xlsx/moskit_companies.xlsx')
     df_companies = df_companies.fillna(0, inplace=False )
-
-
+    
+    # Removendo as linhas duplicadas
+    df_companies.drop_duplicates(inplace=True)
 
     print(df_companies)
 
@@ -33,6 +33,10 @@ def tratamento_dos_dados():
     # CONTACTS
     df_contacts = pd.read_excel('./xlsx/moskit_contacts.xlsx')
     df_contacts = df_contacts.fillna(0, inplace=False)
+    
+    # Removendo as linhas duplicadas
+    df_contacts.drop_duplicates(inplace=True)
+    
     print(df_contacts)
 
     #Salvando o arquivo csv  
@@ -40,7 +44,11 @@ def tratamento_dos_dados():
 
     # CUSTOM_FIELDS
     df_custom_fields = pd.read_excel('./xlsx/moskit_custom_fields.xlsx')
-    df_custom_fields = df_custom_fields.fillna(0, inplace=False , )
+    df_custom_fields = df_custom_fields.fillna(0, inplace=False  )
+    
+    # Removendo as linhas duplicadas
+    
+    df_custom_fields.drop_duplicates(inplace=True)
     print(df_custom_fields)
 
     #Salvando o arquivo csv  
@@ -49,6 +57,9 @@ def tratamento_dos_dados():
     # DEALS
     df_deals = pd.read_excel('./xlsx/moskit_deals.xlsx')
     df_deals = df_deals.fillna(0, inplace=False )
+    
+    # Removendo as linhas duplicadas
+    df_deals.drop_duplicates(inplace=True)
     print(df_deals)
 
     #Salvando o arquivo csv  
@@ -57,7 +68,10 @@ def tratamento_dos_dados():
     # LOSTREASONS
     df_lostreasons = pd.read_excel('./xlsx/moskit_lostreasons.xlsx')
     df_lostreasons = df_lostreasons.fillna(0, inplace=False ) 
-
+    
+    # Removendo as linhas duplicadas
+    df_lostreasons.drop_duplicates(inplace=True)
+    
     #Salvando o arquivo csv            
     df_lostreasons.to_csv('./csv/moskit_lostreasons.csv', index=False)       
 
@@ -65,6 +79,9 @@ def tratamento_dos_dados():
     # PIPELINES
     df_pipelines = pd.read_excel('./xlsx/moskit_pipelines.xlsx')
     df_pipelines = df_pipelines.fillna(0, inplace=False )  
+    
+    # Removendo as linhas duplicadas
+    df_pipelines.drop_duplicates(inplace=True)
 
     #Salvando o arquivo csv  
     df_pipelines.to_csv('./csv/moskit_pipelines.csv', index=False)
@@ -72,16 +89,27 @@ def tratamento_dos_dados():
     # STAGES
     df_stages = pd.read_excel('./xlsx/moskit_stages.xlsx')
     df_stages = df_stages.fillna(0, inplace=False )
-
+    
+    # Removendo as linhas duplicadas
+    df_stages.drop_duplicates(inplace=True)
+    
     #Salvando o arquivo csv                              
     df_stages.to_csv('./csv/moskit_stages.csv', index=False)
 
     # USERS
     df_users = pd.read_excel('./xlsx/moskit_users.xlsx')
     df_users = df_users.fillna(0, inplace=False )
-
+    
+    # Removendo as linhas duplicadas
+    df_users.drop_duplicates(inplace=True)
+    
     #Salvando o arquivo csv  
     df_users.to_csv('./csv/moskit_users.csv', index=False)  
-    
+
+    return  
+
 if __name__ == "__main__":
     tratamento_dos_dados()
+    
+    
+
