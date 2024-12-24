ad0ffb13e7651986e68900d80730188aec479dc4
Guilherme Fernandes Do bem <leciguilherme@gmail.com>
2024-12-24 01:15:51 -0300

    Ranking de vendas
    Ranking  de vendedor que mais vendeu

--- src/utils.py
+++ src/utils.py
@@ -1,1 +1,9 @@
 # Funções auxiliares (cálculos, transformação de dados)
+
+# Ranquear Funil de vendas De onde mais acessou
+# Ranquear Funil de vendas Por quem mais criou contatos
+
+# proximo  deals Open e Closed por vendedor grafico pizza ou barras
+
+# ranque de leads que fechou com o concorrente
+
